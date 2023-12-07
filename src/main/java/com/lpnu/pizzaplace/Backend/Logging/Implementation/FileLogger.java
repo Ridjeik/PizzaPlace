5d6a0cd359bbe76d306182eb3686a68e0993d0d4
Silpux <andymal2439@gmail.com>
2023-12-07 22:11:35 +0200

    [feat] Add timestamp to log message, replace \n with println

--- src/main/java/com/lpnu/pizzaplace/Backend/Logging/Implementation/FileLogger.java
+++ src/main/java/com/lpnu/pizzaplace/Backend/Logging/Implementation/FileLogger.java
@@ -6,16 +6,23 @@ import com.lpnu.pizzaplace.Backend.Logging.Interfaces.Logger;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class FileLogger implements Logger {
 
     private final String filePath = "src\\main\\resources\\PizzaPlaceLog.log";
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
     @Override
     public void log(LogLevel level, String message) {
-        var str = '[' + level.toString() + "] " + message + '\n';
+
+        LocalDateTime timestamp = LocalDateTime.now();
+        String formattedTimestamp = timestamp.format(formatter);
+
+        var str = '[' + level.toString() + "] " + '[' + formattedTimestamp + "] " + message;
 
         try (PrintWriter writer = new PrintWriter(new FileWriter(filePath, true))) {
-            writer.print(str);
+            writer.println(str);
         } catch (IOException e) {
             e.printStackTrace();
         }
